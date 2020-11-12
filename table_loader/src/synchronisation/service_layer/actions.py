e14a82d19a4b3c8183cb0bfc98dd262ba4e98f96
Antonio <antonio.one@pm.me>
2020-11-12 09:45:04 +0000

    created actions skeleton

--- table_loader/src/synchronisation/service_layer/actions.py
+++ table_loader/src/synchronisation/service_layer/actions.py
@@ -1,5 +1,29 @@
-# from table_loader.src.synchronisation.domain import model
+import typing
+
+# from synchronisation.domain import model
+from synchronisation.adapters import repository
+from synchronisation.service_layer import uow
+
+
+def decide_table_action():
+    return "create|replace|update|delete"
+
+
+def decide_actions(preferred_state, last_known, current):
+    actions: typing.Dict[str, str] = {}
+    return actions
 
 
 def synchronise(project: str, bucket_prefix: str):
-    pass
+    with uow:
+        preferred_state = repository.FilesystemGridRepository.list()
+        last_known = repository.GoogleCloudStorageGridRepository.list()
+        current = repository.BigqueryGridRepository.list()
+
+        actions: typing.Dict
+        actions = decide_actions(
+            preferred_state=preferred_state, last_known=last_known, current=current
+        )
+
+        for grid_id, action in actions.items():
+            pass
